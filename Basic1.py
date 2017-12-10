8fc65aaf35c34986b5167fe64361d2185b4dcfe7
sean wang <seanwang03@gmail.com>
2017-12-10 18:39:58 -0500

    basics

--- Basic1.py
+++ Basic1.py
@@ -1,5 +1,7 @@
+import os
 
 print ("hello world")
+
 
 
 f = "abc"
@@ -13,9 +15,29 @@ def myFunc():
     f = "def"
     print (f)
 
+def main():
+    name = input("What is your name?")
+    print ("Your name is " + name)
+
+    for i in range(1,10,2):
+        print (i," ", sep='|', end='') # end='' supress new line for each print. sep='|' user | as seperator
+        #print (i)
+    else:
+        print ("dont printing") # else for for statement. Only execute if all elements have been iterated. 
+
+def print_dir_contents(dir):
+    import os # this is not going to be imported multiple times even if this function is called many times. Because it's cached 
+    print ("dir is " + dir)
+    for child in os.listdir(dir):
+        print(child)
+        if os.path.isdir(child):
+            print_dir_contents(child)
+
 if __name__ == "__main__":
     print("Called from main")
-    main()
+    #main()
+   # prrint_dir_contents(os.path.basename(os.path.dirname(os.path.realpath(__file__))))
+    print_dir_contents(os.path.dirname(os.path.realpath(__file__)))
 else:
     print("Not called from main")
 
@@ -49,3 +71,4 @@ for i in days[2:10]: #NICE slicing notation is :
 #NICE Using emumerate() to get index
 for i, d in enumerate(days):
     print (i,d)
+
